f736a967119d6d7d1c88cc2bc04b36b6046d8388
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-12-29 14:34:06 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+variable "context" {
+  description = <<-EOF
+Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
+
+Examples:
+```
+context:
+  project:
+    name: string
+    id: string
+  environment:
+    name: string
+    id: string
+  resource:
+    name: string
+    id: string
+```
+EOF
+  type        = map(any)
+  default     = {}
+}
+
 variable "test_str" {
   type        = string
   default     = "variable-test"
